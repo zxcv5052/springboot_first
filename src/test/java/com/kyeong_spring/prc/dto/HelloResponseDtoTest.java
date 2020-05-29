5551ee6a7aadfc6faab0f8fea064008c932e3cbc
hyeongseo <luvvboy@gmail.com>
2020-05-30 02:41:23 +0900

    Mapping&Test Education

---
+++ src/test/java/com/kyeong_spring/prc/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.kyeong_spring.prc.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 룸북_기능_테스트(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        Assertions.assertThat(dto.getName()).isEqualTo(name);
+        Assertions.assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
